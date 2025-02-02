247c29d692c2f8fbd8ab15f9b10f71d71bcd9d7d
Youssef <youssefhossam205@gmail.com>
2025-02-02 18:10:08 +0200

    add cors handell

--- src/bootStrap.js
+++ src/bootStrap.js
@@ -1,9 +1,17 @@
+import cors from 'cors'
 import { adminRouter, authRouter, MedicineRouter, OrderRouter } from "./modules/index.js";
 import { globalErrorHandling } from "./utils/appError.js";
 
 export const bootStrap = (app, express) => {
     // parse req
     app.use(express.json());
+    // cors edit
+    const corsOptions = {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    };
+    app.use(cors(corsOptions));
 
     // routing
     app.use("/Medicine", MedicineRouter);
